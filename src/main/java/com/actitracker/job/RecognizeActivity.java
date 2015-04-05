5e71a14ace69d24a3b932f28c6bf929621dada12
Ludwine Probst <ludwine.probst@gmail.com>
2015-04-05 21:09:16 +0200

    add gradle and remove maven

--- src/main/java/com/actitracker/job/RecognizeActivity.java
+++ src/main/java/com/actitracker/job/RecognizeActivity.java
@@ -1,5 +1,25 @@
 package com.actitracker.job;
 
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
 public class RecognizeActivity {
+
+  public static void main(String[] args) {
+    // define Spark context
+    SparkConf sparkConf = new SparkConf()
+        .setAppName("Actitracker")
+        .setMaster("local[*]");
+
+    JavaSparkContext sc = new JavaSparkContext(sparkConf);
+
+    // load the data
+    JavaRDD<String> data = sc.textFile("data.csv");
+
+    System.out.println(data.first());
+    System.out.println(data.count());
+
+  }
 }
